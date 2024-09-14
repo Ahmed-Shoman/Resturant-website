6ad5a96cb8e88a8e7b78fe1389720cb370e646d8
ahmed shoman <147621224+Ahmed-Shoman@users.noreply.github.com>
2024-09-14 18:56:42 +0300

    Update MenuController.php

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MenuStoreRequest;
 use App\Models\Category;
-use App\Models\Menu; // استخدم التسمية الصحيحة للنموذج
+use App\Models\Menu; 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
